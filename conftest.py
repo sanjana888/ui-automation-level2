1c8c7db37da8de85ca8ebfd77796772bd293c902
sanjana888 <sanjanamaroju82@gmail.com>
2026-03-16 14:02:01 +0530

    adding

--- conftest.py
+++ conftest.py
@@ -1,4 +1,3 @@
-import os
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
@@ -7,18 +6,17 @@ from selenium.webdriver.chrome.options import Options
 @pytest.fixture
 def driver():
 
-    grid_url = os.getenv("GRID_URL", "http://localhost:4444/wd/hub")
-
     chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
 
-    driver = webdriver.Remote(
-        command_executor=grid_url,
-        options=chrome_options
-    )
+    driver = webdriver.Chrome(options=chrome_options)
 
     driver.maximize_window()
 
     yield driver
 
     driver.quit()
+
 
